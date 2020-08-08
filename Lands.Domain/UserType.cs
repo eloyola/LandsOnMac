0edd61a62da53273da9c5c1bfc2548d10be98e41
Enrique <enrique.loyola@gmail.com>
2020-08-08 08:45:43 +0100

    update nuget to use image.circle

---
+++ Lands.Domain/UserType.cs
@@ -0,0 +1,18 @@
+﻿namespace Lands.Domain
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class UserType
+    {
+        [Key]
+        public int UserTypeId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [MaxLength(100, ErrorMessage = "The field {0} only can contains a maximum of {1} characters lenght.")]
+        [Index("User_Email_Index", IsUnique = true)]
+        public string Name { get; set; }
+
+    }
+}
